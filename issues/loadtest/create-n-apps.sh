6e8b85c7e3db3c3f2b08c0dca7d27ce9b37037a2
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-01-14 14:57:08 +0100

    Update loadtest scripts

--- issues/loadtest/create-n-apps.sh
+++ issues/loadtest/create-n-apps.sh
@@ -7,10 +7,11 @@ n=100
 
 namespace="loadtest-$n-deployments"
 
-image="jerolimov/nodeinfo"
+s2i="nodejs/14-ubi8"
+repo="https://github.com/jerolimov/nodeinfo"
 
 oc new-project "$namespace"
 
 for i in $(seq $start $(expr $start + $n - 1)); do
-    kubectl -n "$namespace" create deployment "deployment-$i-of-$n" "--image=$image"
+    oc new-app -n "$namespace" "$repo" --name "app-$i-of-$n"
 done
--- issues/loadtest/delete-n-deployments.sh
+++ issues/loadtest/delete-n-deployments.sh
@@ -8,7 +8,7 @@ n=100
 namespace="loadtest-$n-deployments"
 
 for i in $(seq $start $(expr $start + $n - 1)); do
-    kubectl -n "$namespace" delete deployment "deployment-$i"
+    kubectl -n "$namespace" delete deployment "deployment-$i-of-$n"
 done
 
 #oc delete project "$namespace"
