94a9c282f86b440ba22a9bcea85b5a600eb5421a
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-06-27 08:27:54 +0200

    Add 4.12

--- scripts/bridge-4.11.sh
+++ scripts/bridge-4.11.sh
@@ -3,4 +3,3 @@ export KUBECONFIG=~/.crc/machines/crc/kubeconfig && \
 oc login -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password) --server https://api.crc.testing:6443 && \
 source ./contrib/oc-environment.sh && \
 bin/bridge -listen http://0.0.0.0:9011 -user-settings-location configmap
-
--- scripts/bridge-4.12.sh
+++ scripts/bridge-4.12.sh
@@ -1,5 +1,5 @@
-cd ~/git/openshift/console-4.8 && \
+cd ~/git/openshift/console-4.12 && \
 export KUBECONFIG=~/.crc/machines/crc/kubeconfig && \
 oc login -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password) --server https://api.crc.testing:6443 && \
 source ./contrib/oc-environment.sh && \
-bin/bridge -listen http://0.0.0.0:9008 -user-settings-location configmap
+bin/bridge -listen http://0.0.0.0:9012 -user-settings-location configmap
