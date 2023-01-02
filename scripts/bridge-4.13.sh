2c048e04d8fc707d0dad8f43e46a20a814700df2
Christoph Jerolimov <jerolimov+github@redhat.com>
2023-01-03 00:11:55 +0100

    Add new script to start local 4.13

---
+++ scripts/bridge-4.13.sh
@@ -0,0 +1,5 @@
+cd ~/git/openshift/console-4.13 && \
+export KUBECONFIG=~/.crc/machines/crc/kubeconfig && \
+oc login -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password) --server https://api.crc.testing:6443 && \
+source ./contrib/oc-environment.sh && \
+bin/bridge -listen http://0.0.0.0:9013 -user-settings-location configmap
